411aa5f8e698e32e94865d3808d0855e7ffbba2a
lyndaflower <umurerwalynda@gmail.com>
2020-08-25 07:09:55 +0200

    make a change

--- JS/blog.js
+++ JS/blog.js
@@ -1,70 +1,77 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAG6etKCA1boqukqISl8uJYsc3hqXKANUQ",
-    authDomain: "my-brand-lynda.firebaseapp.com",
-    databaseURL: "https://my-brand-lynda.firebaseio.com",
-    projectId: "my-brand-lynda",
-    storageBucket: "my-brand-lynda.appspot.com",
-    messagingSenderId: "1061558652166",
-    appId: "1:1061558652166:web:8e422f69045c82839a2287",
-    measurementId: "G-QB0ZDPQCTH",
-  };
-  firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+  apiKey: "AIzaSyDsZzR0HFcDDtdVEK-SvkVbcOpC8HjJ0yQ",
+  authDomain: "personal-website-4f106.firebaseapp.com",
+  databaseURL: "https://personal-website-4f106.firebaseio.com",
+  projectId: "personal-website-4f106",
+  storageBucket: "personal-website-4f106.appspot.com",
+  messagingSenderId: "374502379076",
+  appId: "1:374502379076:web:2bbbd4096010aef7d37611",
+  measurementId: "G-XL6FTWZPBP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db=firebase.firestore();
+db.settings({});
+db.collection('blogs').get().then((snapshot) =>{
+snapshot.docs.forEach(docs => {
+  console.log(docs.data());
+});
+})
   
-  const dbRef = firebase.database().ref();
-  const blogsRef = dbRef.child("blogs");
+  // const dbRef = firebase.database().ref();
+  // const blogsRef = dbRef.child("blogs");
   
 
-const loadAllArticles = () => {
-    const blogsRef = firebase.database().ref("blogs");
-    var blogSection = document.querySelector("#blog-section");
-    blogsRef.on("child_added", (snap) => {
+// const loadAllArticles = () => {
+//     const blogsRef = firebase.database().ref("blogs");
+//     var blogSection = document.querySelector("#blog-section");
+//     blogsRef.on("child_added", (snap) => {
 
-      // let currentBlog = snap.key;
-      // localStorage.setItem("dataKey", snap.key);
-      // console.log(currentBlog);
+//       // blog-card
+//       let blogCard = document.createElement("div");
+//       let blogDetails = document.createElement("div");
+//       let blogDesc = document.createElement("h2");
+//       blogDesc.innerHTML = snap.val().description;
+//       let blogBody = document.createElement("p");
+//       blogBody.innerHTML = snap.val().body;
 
+//         // blog-cover-image
+//       let blogImage = document.createElement("div");
+//       let image = document.createElement('img');
+//       image.src = snap.val().image;
 
-      // blog-card
-      let blogCard = document.createElement("div");
-      let blogDetails = document.createElement("div");
-      let blogDesc = document.createElement("h2");
-      blogDesc.innerHTML = snap.val().description;
-      let blogBody = document.createElement("p");
-      blogBody.innerHTML = snap.val().body.slice(0,150);
+//       // const theImage="<img src='" + snap.val().image +"/>";
+//       //  if(snap.val.image) blogImage.innerHTML += theImage;
 
-       //  blog-cover-image
-      let blogImage = document.createElement("div");
-      let image = document.createElement('img');
-      image.src = snap.val().image;
-       
-      // blog title
-      let blogTitle = document.createElement("h1");
-      blogTitle.innerHTML = snap.val().title;
+//       // blog title
+//       let blogTitle = document.createElement("h1");
+//       blogTitle.innerHTML = snap.val().title;
 
-      // blog-readmore
-      let blogReadMore = document.createElement("a");
-      blogReadMore.setAttribute("href", "./article-list.html");
-      const readMore = document.createTextNode("read more");
-      blogReadMore.appendChild(readMore);
-      blogReadMore.addEventListener('click', ()=>{
-        localStorage.setItem('dataKey', snap.key);
-      })
+//       // blog-readmore
+//       let blogReadMore = document.createElement("a");
+//       blogReadMore.setAttribute("href", "./article-list.html");
+//       const readMore = document.createTextNode("read more");
+//       blogReadMore.appendChild(readMore);
+//       blogReadMore.addEventListener('click', ()=>{
+//         localStorage.setItem('dataKey', snap.key);
+//       });
 
-      image.setAttribute('class', 'photo');
-      blogCard.setAttribute('class', 'blog-card');
-      blogImage.setAttribute('class','meta');
-      blogDetails.setAttribute('class','description');
-      blogReadMore.setAttribute('class','read-more a');
+//       image.setAttribute('class', 'photo');
+//       blogCard.setAttribute('class', 'blog-card');
+//       blogImage.setAttribute('class','meta');
+//       blogDetails.setAttribute('class','description');
+//       blogReadMore.setAttribute('class','read-more a');
 
-      blogImage.append(image);
-      blogDetails.append(blogTitle);
-      blogDetails.append(blogDesc);
-      blogDetails.append(blogBody);
-      blogDetails.append(blogReadMore);
+//       blogImage.append(image);
+//       blogDetails.append(blogTitle);
+//       blogDetails.append(blogDesc);
+//       blogDetails.append(blogBody);
+//       blogDetails.append(blogReadMore);
   
   
-      blogCard.append(blogDetails);
-      blogCard.append(blogImage);
-      blogSection.append(blogCard);
-    });
-  };
+//       blogCard.append(blogDetails);
+//       blogCard.append(blogImage);
+//       blogSection.append(blogCard);
+//     });
+//   };
--- JS/init-firebase.js
+++ JS/init-firebase.js
@@ -1,57 +1,81 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAG6etKCA1boqukqISl8uJYsc3hqXKANUQ",
-    authDomain: "my-brand-lynda.firebaseapp.com",
-    databaseURL: "https://my-brand-lynda.firebaseio.com",
-    projectId: "my-brand-lynda",
-    storageBucket: "my-brand-lynda.appspot.com",
-    messagingSenderId: "1061558652166",
-    appId: "1:1061558652166:web:8e422f69045c82839a2287",
-    measurementId: "G-QB0ZDPQCTH",
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyDsZzR0HFcDDtdVEK-SvkVbcOpC8HjJ0yQ",
+  authDomain: "personal-website-4f106.firebaseapp.com",
+  databaseURL: "https://personal-website-4f106.firebaseio.com",
+  projectId: "personal-website-4f106",
+  storageBucket: "personal-website-4f106.appspot.com",
+  messagingSenderId: "374502379076",
+  appId: "1:374502379076:web:2bbbd4096010aef7d37611",
+  measurementId: "G-XL6FTWZPBP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+db.settings({});
+
+db.collection('blogs').get().then((snapshot) =>{
+snapshot.docs.forEach(docs => {
+  console.log(docs.data());
+});
+})
+
+const  createArticle= document.getElementsByClassName(".article-input");
+// saving data
+
+const addBlogBtnClicked =()=>{
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+  let body = document.getElementById("body").value;
+  db.collection("blogs").docs().set({
+    title:title,
+    description:description,
+    body:body
+  })
+  .then(function() {
+         console.log("Doc successful");
+     })
+     .catch(function(error) {
+        console.error("Error writing doc", error);
+     });
+}
+  // const addBlogBtnUI = document.getElementById("add-blog-btn");
+  // var imageUrls 
+  // const chooseFile=() => {
+  //   // to get value
+  //   const { files } = document.querySelector('input[type="file"]');
+  //   // console.log("value", files[0]);
   
-  firebase.initializeApp(firebaseConfig);
+  //   var storageRef = firebase.storage().ref(files[0].name);
+  //   let uploadTask = storageRef.put(files[0]);
+  //   // console.log("upload image", uploadTask);
   
-  const dbRef = firebase.database().ref();
-  const blogsRef = dbRef.child("blogs");
-  const addBlogBtnUI = document.getElementById("add-blog-btn");
-  var imageUrls 
-  const upload = () => {
-    // to get value
-    const { files } = document.querySelector('input[type="file"]');
-    console.log("value", files[0]);
+  //   uploadTask.then((snap) => {
+  //     // console.log(snap.metadata.downloadURLs);
+  //     imageUrls = snap.metadata.downloadURLs;
+  //   });
+  // };
   
-    var storageRef = firebase.storage().ref(files[0].name);
-    let uploadTask = storageRef.put(files[0]);
-    console.log("upload image", uploadTask);
+  // const addBlogBtnClicked = () => {
+   
   
-    uploadTask.then((snap) => {
-      console.log(snap.metadata.downloadURLs);
-      imageUrls = snap.metadata.downloadURLs
-    });
-  };
+  //   // this object will hold the new user information
+  //   let newBlog = {};
   
-  const addBlogBtnClicked = () => {
-    const addUserInputsUI = document.getElementsByClassName("article-input");
+  //   // loop through View to get the data for the model
+  //   for (let i = 0, len = addUserInputsUI.length; i < len; i++) {
+  //     let key = addUserInputsUI[i].getAttribute("name");
+  //     let value = addUserInputsUI[i].value;
+  //     newBlog[key] = value;
+  //   }
   
-    // this object will hold the new user information
-    let newBlog = {};
+  //   var blog = {...newBlog, image:imageUrls}
+  //   console.log(blog);
+  //   blogsRef.push(blog, function () {
+  //     console.log("data has been inserted");
+  //   });
+  // };
+  // addBlogBtnUI.addEventListener("click", addBlogBtnClicked);
   
-    // loop through View to get the data for the model
-    for (let i = 0, len = addUserInputsUI.length; i < len; i++) {
-      let key = addUserInputsUI[i].getAttribute("name");
-      let value = addUserInputsUI[i].value;
-      newBlog[key] = value;
-    }
-  
-    var blog = {...newBlog, image:imageUrls}
-    console.log(blog);
-    blogsRef.push(blog, function () {
-      console.log("data has been inserted");
-    });
-  };
-  addBlogBtnUI.addEventListener("click", addBlogBtnClicked);
-  
-  const uploadBtn = document.querySelector("#upload");
-  uploadBtn.addEventListener("click", upload);
-  
-  
+  // const uploadBtn = document.querySelector("#upload");
+  // uploadBtn.addEventListener("click", upload);
