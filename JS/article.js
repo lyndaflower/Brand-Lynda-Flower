e6302d64ff58fa1fc546125c7fb9a223567080a1
lyndaflower <umurerwalynda@gmail.com>
2020-08-26 10:42:09 +0200

    display single article

---
+++ JS/article.js
@@ -0,0 +1,36 @@
+import {db} from "./config.js";
+import {storage} from "./config.js";
+
+const singleArticleId = localStorage.getItem("article-id");
+db.collection('blogs').doc(singleArticleId).get().then(snapshot=>{
+    // console.log(snapshot.data().url);
+    const articlesSection = document.querySelector("section.article");
+    const article = document.createElement("article");
+    const blogTitle = document.createElement("h2");
+    const blogDescription = document.createElement("h2");
+    const articleImage = document.createElement("img");
+    const blogBody = document.createElement("p");
+
+   
+    const articleTitle = document.createTextNode(snapshot.data().title);
+    const articleBody = document.createTextNode(snapshot.data().body);
+    const articleDescription = document.createTextNode(snapshot.data().description);
+    articleImage.src = snapshot.data().url;
+
+    blogTitle.setAttribute("class", "article-title");
+    blogDescription.setAttribute("class","desc");
+    blogBody.setAttribute("class", "article-body");
+    articleImage.setAttribute("class","image");
+    articleImage.setAttribute("alt", "image");
+
+    blogTitle.appendChild(articleTitle);
+    article.appendChild(blogTitle);
+    blogDescription.appendChild(articleDescription);
+    article.appendChild(blogDescription);
+    article.appendChild(articleImage);
+    blogBody.appendChild(articleBody);
+    article.appendChild(blogBody);
+   
+    articlesSection.appendChild(article);
+})
+
--- JS/blog.js
+++ JS/blog.js
@@ -1,60 +1,60 @@
-import {db} from "./config.js";
+import { db } from "./config.js";
 // import {storage} from "./config";
-  
-db.collection('blogs').get().then((snapshot) =>{
-  blogsRef.on("child_added", (snap)
-  snapshot.docs.forEach(docs => {
-    
-      // blog-card
-      let blogCard = document.createElement("div");
-      let blogDetails = document.createElement("div");
-      let blogDesc = document.createElement("h2");
-      blogDesc.innerHTML = docs.data().description;
-      let blogBody = document.createElement("p");
-      blogBody.innerHTML = docs.data().body;
 
-        // blog-cover-image
-      let blogImage = document.createElement("div");
-      let image = document.createElement('img');
-      image.src = docs.data().image;
 
-      // const theImage="<img src='" + snap.val().image +"/>";
-      //  if(snap.val.image) blogImage.innerHTML += theImage;
 
-      // blog title
-      let blogTitle = document.createElement("h1");
-      blogTitle.innerHTML = docs.data().title;
+const loadAllArticles = (docs) => {
+  var blogSection = document.querySelector("#blog-section");
 
-      // blog-readmore
-      let blogReadMore = document.createElement("a");
-      blogReadMore.setAttribute("href", "./article-list.html");
-      const readMore = document.createTextNode("read more");
-      blogReadMore.appendChild(readMore);
-      blogReadMore.addEventListener('click', ()=>{
-        localStorage.setItem('dataKey', snap.key);
-      });
+  const singleArticle = (id) => {
+    localStorage.setItem("article-id", docs.id);
+    window.location.assign("article-list.html");
+  };
+  // blog-card
+  let blogCard = document.createElement("div");
+  let blogDetails = document.createElement("div");
+  let blogDesc = document.createElement("h2");
+  blogDesc.innerHTML = docs.data().description;
+  let blogBody = document.createElement("p");
+  blogBody.innerHTML = `${docs.data().body.slice(0, 150)}...  `;
 
-      image.setAttribute('class', 'photo');
-      blogCard.setAttribute('class', 'blog-card');
-      blogImage.setAttribute('class','meta');
-      blogDetails.setAttribute('class','description');
-      blogReadMore.setAttribute('class','read-more a');
+  // blog-cover-image
+  let image = document.createElement("img");
+  let blogImage = document.createElement("div");
+  image.src = docs.data().url;
 
-      blogImage.append(image);
-      blogDetails.append(blogTitle);
-      blogDetails.append(blogDesc);
-      blogDetails.append(blogBody);
-      blogDetails.append(blogReadMore);
-  
-  
-      blogCard.append(blogDetails);
-      blogCard.append(blogImage);
-      blogCard.append(blogCard);
+  // blog title
+  let blogTitle = document.createElement("h1");
+  blogTitle.innerHTML = docs.data().title;
+
+  // blog-readmore
+  let blogReadMore = document.createElement("a");
+  const readMore = document.createTextNode("read more");
+  blogReadMore.appendChild(readMore);
+  blogReadMore.addEventListener('click', () =>
+    singleArticle(docs.id));
+
+  image.setAttribute("class", "photo");
+  blogCard.setAttribute("class", "blog-card");
+  blogImage.setAttribute("class", "meta");
+  blogDetails.setAttribute("class", "description");
+  blogReadMore.setAttribute("class", "read-more a");
+
+  blogImage.append(image);
+  blogDetails.append(blogTitle);
+  blogDetails.append(blogDesc);
+  blogDetails.append(blogBody);
+  blogDetails.append(blogReadMore);
+
+  blogCard.append(blogDetails);
+  blogCard.append(blogImage);
+  blogSection.append(blogCard);
+};
+
+db.collection("blogs")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((docs) => {
+      loadAllArticles(docs);
     });
-  })
-
-// const loadAllArticles = () => {
-//     var blogSection = document.querySelector("#blog-section");
-//     blogsRef.on("child_added", (snap) => {
-
-//   };
+  });
